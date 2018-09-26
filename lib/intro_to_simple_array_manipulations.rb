b524881f82a84a868a8748f58972fb406e450cf1
Sotom591 <sotom591@gmail.com>
2018-09-26 02:10:19 -0400

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,50 @@
+def using_push(array, string)
+updated_array = string
+array.push(updated_array)
+end
+
+def using_unshift(array, string)
+array.unshift(string)
+end
+
+def using_pop(array)
+array.pop
+end
+
+def pop_with_args(array)
+array.pop(2)
+end
+
+def using_shift(array)
+array.shift
+end
+
+def shift_with_args(array)
+array.shift(2)
+end
+
+def using_concat(array1, array2)
+array1.concat(array2)
+end
+
+def using_insert(list_of_esoteric_programming_languages, another_esoteric_language)
+another_esoteric_language = list_of_esoteric_programming_languages.insert(4, "Malbolge")
+end
+
+def using_uniq(array)
+array.uniq
+end
+
+def using_flatten(private_colleges_in_newyork)
+flat_array = [private_colleges_in_newyork]
+flat_array.flatten
+
+end
+
+def using_delete(array, string)
+array.delete(string)
+end
+
+def using_delete_at(famous_robots, integer)
+famous_robots.delete_at(2)
+end
